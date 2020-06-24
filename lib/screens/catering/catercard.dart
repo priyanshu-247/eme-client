a8869974c087b8d11657d2d65d8bed1b3f0d6ee2
codeweb-stack <codeweb.stack@gmail.com>
2020-06-24 22:45:15 +0530

    min-ui-change

--- lib/screens/catering/catercard.dart
+++ lib/screens/catering/catercard.dart
@@ -18,33 +18,39 @@ class CaterCard extends StatelessWidget {
             .navigate("/caterdetail", args: Caters(caterId: caters.caterId));
       },
       child: Container(
-        margin: EdgeInsets.symmetric(vertical: 12.0),
-        padding: EdgeInsets.symmetric(horizontal: 25.0),
+        margin: EdgeInsets.symmetric(vertical: 5.0),
+        padding: EdgeInsets.symmetric(horizontal: 10.0),
         child: Column(
           children: <Widget>[
-            Container(
-              height: 200,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage(caters.imageUrl),
-                    fit: BoxFit.cover,
-                  ),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10.0),
-                    topRight: Radius.circular(10.0),
-                  )),
+            Card(
+              elevation: 5,
+              child: Container(
+                height: 200,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(caters.imageUrl),
+                      fit: BoxFit.cover,
+                    ),
+                    border: Border.all(width: 4.0, color: Colors.red),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10.0),
+                      topRight: Radius.circular(10.0),
+                    )),
+              ),
             ),
             Container(
               padding: EdgeInsets.all(10.0),
               decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(10.0),
-                      bottomRight: Radius.circular(10.0)),
-                  boxShadow: [
-                    BoxShadow(
-                        blurRadius: 2.0, spreadRadius: 1.0, color: Colors.grey)
-                  ]),
+                color: Colors.white,
+                border: Border.all(width: 3.0, color: Colors.redAccent),
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(10.0),
+                    bottomRight: Radius.circular(10.0)),
+                // boxShadow: [
+                //   BoxShadow(
+                //       blurRadius: 2.0, spreadRadius: 1.0, color: Colors.grey)
+                // ]
+              ),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
@@ -57,12 +63,18 @@ class CaterCard extends StatelessWidget {
                         style: TextStyle(
                             fontWeight: FontWeight.bold, fontSize: 16.0),
                       ),
+                      SizedBox(
+                        height: 10,
+                      ),
                       Text(
                         caters.catersAbout.toLowerCase(),
                         style: TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 12.0,
                             color: Colors.grey),
+                      ),
+                      SizedBox(
+                        height: 10,
                       ),
                       Padding(
                         padding: const EdgeInsets.only(top: 8),
